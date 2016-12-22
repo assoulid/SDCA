c319080477589ea887e2267fb42f83fb08e1d027
OthmaneN <nahyl.othmane@gmail.com>
2016-12-22 18:52:19 +0100

    Added test b service

---
+++ cloud_native_app/microservices/b/b.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""Testing service"""
+
+import logging
+import os
+import pprint
+import sys
+from logging.handlers import RotatingFileHandler
+
+from flask import Flask
+from flask import jsonify
+
+import config
+
+# Initialise Flask
+app = Flask(__name__)
+app.debug = True
+
+# Affect app logger to a global variable so logger can be used elsewhere.
+config.logger = app.logger
+
+
+@app.route("/", methods=["GET"])
+def api_root():
+    """Root url, provide service name and version. Used for consul health checks"""
+    data = {
+        "Service": config.w.NAME,
+        "Version": config.w.VERSION
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    resp.headers["AuthorSite"] = "https://github.com/uggla/openstack_lab"
+
+    add_headers(resp)
+    return resp
+
+
+@app.route("/test")
+def api_test():
+    config.logger.info("*** Start testing ***")
+    data = {"message": "Hello"}
+    resp = jsonify(data)
+    resp.status_code = 200
+    config.logger.info("*** End testing")
+    add_headers(resp)
+    return resp
+
+
+def configure_logger(logger, logfile):
+    """Configure logger"""
+    formatter = logging.Formatter(
+        "%(asctime)s :: %(levelname)s :: %(message)s")
+    file_handler = RotatingFileHandler(logfile, "a", 1000000, 1)
+
+    # Add logger to file
+    if config.b.conf_file.get_b_debug().title() == 'True':
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+
+def add_headers(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type,Authorization')
+
+
+if __name__ == "__main__":
+    # Vars
+    app_logfile = "b.log"
+
+    # Change diretory to script one
+    try:
+        os.chdir(os.path.dirname(sys.argv[0]))
+    except FileNotFoundError:
+        pass
+
+    # Define a PrettyPrinter for debugging.
+    pp = pprint.PrettyPrinter(indent=4)
+
+    # Initialise apps
+    config.initialise_b()
+
+    # Configure Flask logger
+    configure_logger(app.logger, app_logfile)
+
+    config.logger.info("Starting %s", config.b.NAME)
+    app.run(port=int(config.b.conf_file.get_b_port()), host='0.0.0.0')
---
+++ cloud_native_app/microservices/b/config.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+
+import configparser
+
+# Initialise global variable
+logger = None
+b = None
+
+
+def initialise_b():
+    """Define w global object so it can be called from anywhere."""
+    global b
+    b = B()
+
+
+class B(object):
+    def __init__(self):
+        self.NAME = "Microservice b"
+        self.VERSION = "0.1"
+        #
+        # Configuration file
+        self.conf_file = BConfiguration("b.conf")
+
+
+class BConfiguration(object):
+    def __init__(self, configuration_file):
+        self.config = configparser.ConfigParser(allow_no_value=True)
+        self.config.read(configuration_file)
+
+    def get_b_port(self):
+        return self.config.get("b", "port")
+
+    def get_b_tmpfile(self):
+        return self.config.get("b", "tmpfile")
+
+    def get_b_tempo(self):
+        return self.config.get("b", "tempo")
+
+    def get_b_debug(self):
+        return self.config.get("b", "debug")
