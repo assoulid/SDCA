eb92e82d656014ef2c1eb25b05de4f4941aefb83
OthmaneN <nahyl.othmane@gmail.com>
2016-12-25 16:17:58 +0100

    microservices playbooks run asynchronously

--- scripts/run_ansible_playbooks.sh
+++ scripts/run_ansible_playbooks.sh
@@ -5,13 +5,16 @@ rm ~/.ssh/known_hosts
 ~/scan_hosts
 
 #Run common playbook
-ansible-playbook ../playbooks/all_nodes.yaml --ssh-common-args='-o IdentityFile=~/.ssh/cle_bastion'
+echo 'Running all_nodes.yaml playbook'
+ansible-playbook ../playbooks/all_nodes.yaml --ssh-common-args='-o IdentityFile=~/.ssh/cle_bastion' > all_nodes.out
 
 #Run consul_server playbook
-ansible-playbook ../playbooks/consul_server.yaml --ssh-common-args='-o IdentityFile=~/.ssh/cle_bastion'
+echo 'Running consul_server.yaml playbook'
+ansible-playbook ../playbooks/consul_server.yaml --ssh-common-args='-o IdentityFile=~/.ssh/cle_bastion' > consul_server.out
 
+echo 'Running w.yaml and b.yaml playbooks in parallel'
 #Run w playbook
-ansible-playbook ../playbooks/w.yaml --ssh-common-args='-o IdentityFile=~/.ssh/cle_bastion'
+ansible-playbook ../playbooks/w.yaml --ssh-common-args='-o IdentityFile=~/.ssh/cle_bastion' > w.out &
 
 #Run b playbook
-ansible-playbook ../playbooks/b.yaml --ssh-common-args='-o IdentityFile=~/.ssh/cle_bastion'
+ansible-playbook ../playbooks/b.yaml --ssh-common-args='-o IdentityFile=~/.ssh/cle_bastion' > b.out &
