14fc574c47e7b90a7931d14c66e1a61ae0eefccd
Baptiste Giroux <baptiste.giroux@ensimag.grenoble-inp.fr>
2017-01-03 17:16:20 +0100

    ajout requete appli->serviceS

--- partie_dev/appliWeb/app/config.ts
+++ partie_dev/appliWeb/app/config.ts
@@ -2,6 +2,7 @@ export class Config{
 
     static addrServiceI:string = "http://localhost:3210";
     static addrServiceP:string = "";
+    static addrServiceS:string = "http://localhost:3220";
     static addrServiceB:string = "";
 
 }
--- partie_dev/appliWeb/app/microServiceB.ts
+++ partie_dev/appliWeb/app/microServiceB.ts
@@ -1,4 +1,10 @@
-import {Component} from "@angular/core";
+import {Component, Injectable} from "@angular/core";
+import {Utils} from "./utils";
+import {Http,Headers} from '@angular/http';
+import {Config} from './config';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/timeout';
+
 
 @Component({
    selector: 'micro-service-B',
@@ -7,6 +13,64 @@ import {Component} from "@angular/core";
 })
 
 export class MicroServiceB {
+
+
+    id = -1;
+    error = false;
+    errorMsg = "";
+
+    aJoue : boolean = false;
+
+    constructor(private http: Http){
+       let aux:string = Utils.getParameterByName("id");
+        console.log("Param id : "+aux)
+        if(aux != null && aux !== ""){
+            this.id = +aux;
+        }
+    }
+
+
+	getStatus(id:string){
+		let method = '/status'+'/'+id;
+		let urlws : string= Config.addrServiceS + method ;
+		let headers = new Headers();
+		headers.append('Content-Type', 'application/json');
+
+		console.log("Acces à l'adresse : "+urlws);
+		return this.http.get(urlws,{headers:headers})
+			.timeout(5000)
+            .map(res => res.json())
+			.map((res: any) => {
+				return res;
+			})
+	}
+
+    serviceButton(id:string){
+        let method = '/play'+'/'+id;
+		let urlws : string= Config.addrServiceB + method ;
+		let headers = new Headers();
+		headers.append('Content-Type', 'application/json');
+
+		console.log("Acces à l'adresse : "+urlws);
+		return this.http.get(urlws,{headers:headers})
+			.timeout(5000)
+            .map(res => res.json())
+			.map((res: any) => {
+				return res;
+			})
+    }
+
+	jouer(){
+		this.serviceButton(this.id.toString()).subscribe(
+			data=>{
+                console.log("OK");
+			},
+			error=>{
+				console.log("Le service B ne répond pas...");
+			}
+		);
+	}
+
 
 
 }
