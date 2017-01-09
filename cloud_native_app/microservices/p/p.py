415cc987e1203d8df484e9c7247879a39b1f0992
Nicolas Danet <nicolas.danet@ensimag.grenoble-inp.fr>
2017-01-09 15:40:59 +0100

    Microservice p

--- cloud_native_app/microservices/p/p.py
+++ cloud_native_app/microservices/p/p.py
@@ -1,19 +1,34 @@
 import swiftclient
 
-user = 'username'
-password = 'password'
-authurl ='http://10.11.50.26:5000/v3/auth'
+# TODO : retirer le mot de passe en clair
+user = 'groupe4'
+password = 'zH0lJRtRhVU='
+authurl ='http://10.11.50.26:5000/v3/auth/tokens'
 
-try:
-    swift_connexion = swiftclient.client.Connection(
-    	authurl=authurl, 
-    	user=user, 
-    	key=password 
-	)
-    object_body = swift_connexion.get_object('prizes_container', id)
-    swift_connexion.close()
-    f = open('prize.png', 'wb')
-    f.write(object_body)
-    f.close()
-except swiftclient.exceptions.ClientException:
-    print(traceback.format_exc())
+@app.route("/<id>"); 
+def swift_get(id):
+    try:
+        swift_connexion = swiftclient.client.Connection(
+        	authurl=authurl, 
+        	user=user, 
+        	key=password 
+    	)
+        object_body = swift_connexion.get_object('prizes_container', id)
+        swift_connexion.close()
+
+        # decodage de l'image
+        
+        img = object_body.encode("ascii");
+        img = base64.b64decode(img);
+
+        resp = jsonify(object_body)
+        resp.status_code = 200
+
+        resp.headers["Content-Type"] = "image/jpeg"
+
+        add_headers(resp)
+        return resp
+
+    except swiftclient.exceptions.ClientException:
+        print(traceback.format_exc())
+    
