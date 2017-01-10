6b4696b94073de41cd9239761b9fbbf63eea194c
Daniel <danassouline@hotmail.fr>
2017-01-10 17:55:24 +0100

    ajoutd de la requete pour recuperer l'ip de la bd mysql

--- cloud_native_app/microservices/s/main.js
+++ cloud_native_app/microservices/s/main.js
@@ -1,28 +1,7 @@
 var express = require('express');
 var app = express();
 var cors = require('cors');
-YAML = require('yamljs');
-
-// Loading configuration of database
-console.log("Loading configuration of database ...");
-config = YAML.load('./config.yml');
-
-/********** DATABASE **********/
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : config.host,
-  user     : config.user,
-  password : config.password,
-  database : config.database
-});
-
-connection.connect(function(err){
-if(!err) {
-    console.log("Database is connected ...");
-} else {
-    console.log("Error connecting database ...");
-}
-});
+var request = require("request");
 
 /********** HEADER **********/
 app.use(function(req, res, next) {
@@ -43,22 +22,54 @@ app.get('/status/:id', function (req, res) {
   var id = req.params.id;
   console.log("requete GET : status/"+id)
 
-  if (id < 20002 && id > 0) {
-    result.id = id;
-    connection.query('SELECT has_played FROM has_played WHERE id_customer='+id, function(err, rows, fields) {
-      // Closing connection
-      // connection.end();
-      if (!err) {
-        // console.log(rows[0].has_played);
-        aJoue = rows[0].has_played;
-        result.aJoue = !!+aJoue; // Converts int to boolean
-        res.send(JSON.stringify(result));
-      }
-      else
-        console.log('Error while performing Query.');
-    });
-  }
+  // Loading database IP and port
+  request("http://localhost:8500/v1/catalog/service/mysql", function (error, response, body) {
 
+    console.log(body);
+
+    if (error) {
+        console.log(error)
+    } else {
+      host = JSON.parse(body)[0]["Address"];
+      port = JSON.parse(body)[0]["ServicePort"];
+
+      console.log(host, port, "\n");
+
+      // Connection to database
+      var connection = mysql.createConnection({
+        host: host,
+        port: port,
+        user: 'root',
+        password: 'group4',
+        database: 'prestashop'
+      });
+
+      connection.connect(function (err) {
+        if (!err) {
+            console.log("Database is connected ... nn");
+        } else {
+            console.log(err);
+        }
+      });
+
+      // Executing query
+      if (id < 20002 && id > 0) {
+        result.id = id;
+        connection.query('SELECT has_played FROM has_played WHERE id_customer='+id, function(err, rows, fields) {
+          // Closing connection
+          // connection.end();
+          if (!err) {
+            // console.log(rows[0].has_played);
+            aJoue = rows[0].has_played;
+            result.aJoue = !!+aJoue; // Converts int to boolean
+            res.send(JSON.stringify(result));
+          }
+          else
+            console.log('Error while performing Query.');
+        });
+      }
+    }
+  });
 });
 
 var port = 3220
