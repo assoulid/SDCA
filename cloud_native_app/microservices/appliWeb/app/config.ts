fe0afe34b74273b20c106e10d38a2fa12dcecf5b
Baptiste Giroux <baptiste.giroux@ensimag.grenoble-inp.fr>
2017-01-10 16:48:30 +0100

    suppression de fichiers inutiles

--- cloud_native_app/microservices/appliWeb/app/config.ts
+++
@@ -1,8 +0,0 @@
-export class Config{
-
-    static addrServiceI:string = "http://localhost:3210";
-    static addrServiceP:string = "";
-    static addrServiceS:string = "http://localhost:3220";
-    static addrServiceB:string = "";
-
-}
