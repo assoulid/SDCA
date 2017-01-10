e441378ff50397bb8489b048628e40e16ae03623
Baptiste Giroux <baptiste.giroux@ensimag.grenoble-inp.fr>
2017-01-10 11:49:38 +0100

    debug requete appliweb->b

--- cloud_native_app/microservices/appliWeb/index.js
+++ cloud_native_app/microservices/appliWeb/index.js
@@ -1,39 +1,10 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var http = require("http");
+var request = require("request")
 
 
-function getOptionsDNS(serviceName){
-    return {
-        host: 'localhost',
-        port: '8500',
-        path: '/v1/catalog/service/'+serviceName,
-        method: 'GET',
-        headers: {'Content-Type': 'application/json'}
-    };
-}
-
-function httpRequest(options, onResult,onError)
-{
-    var req = http.request(options, function(res)
-    {
-        var output = '';
-        console.log(options.host + ':' + res.statusCode);
-        res.setEncoding('utf8');
-
-        res.on('data', function (chunk) {
-            output += chunk;
-        });
-
-        res.on('end', function() {
-            var obj = JSON.parse(output);
-            onResult(res.statusCode, obj);
-        });
-    });
-    req.on('error', onError);
-    req.end();
-};
+var DNSAddr = "http://localhost:8500/v1/catalog/service/"
 
 
 
@@ -44,7 +15,7 @@ app.get('/', function (req, res) {
 app.get('/play/:id', function(req, res) {
 
     console.log('play : '+req.params.id);
-
+/*
     var treatement = function(status,obj){
         if(obj!=null && obj.length > 0){
             var id = req.params.id;
@@ -62,6 +33,30 @@ app.get('/play/:id', function(req, res) {
     var optionsDns = getOptionsDNS('b');
     console.log("http : "+JSON.stringify(optionsDns))
     httpRequest(getOptionsDNS('b'), treatement, function(error){console.log(JSON.stringify(error))})
+
+*/
+    request(DNSAddr + "b", function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body); // Show the HTML for the Modulus homepage.
+            if(b && b.length > 0){
+                var addrB = b[0]["Address"]
+                var portB = b[0]["ServicePort"]
+                request('http://'+addrB+":"+portB+'/play/'+id, function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        console.log(body); // Show the HTML for the Modulus homepage.
+                        res.send(body)
+                    }else{
+                        console.log(error)
+                        res.send(error)
+                    }
+                });
+            }
+        }else{
+            console.log(error)
+        }
+    });
+
+
 });
 
 app.use(express.static(__dirname));
