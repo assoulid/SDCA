246a67add534809ee5b86c39dd61d88b1fa45bb5
BABOU\Baptiste <baptiste.giroux@ensimag.grenoble-inp.fr>
2017-01-17 12:05:15 +0100

    erreur import

---
+++ cloud_native_app/microservices/appliWeb/app/dns.ts
@@ -0,0 +1,36 @@
+import {Component, Injectable} from "@angular/core";
+import {Http,Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/timeout';
+
+
+
+@Injectable()
+export class Dns {
+    
+    host:string;
+
+    constructor(private http: Http){ 
+        this.host = window.location.protocol+"//"+window.location.hostname;
+        if(window.location.port){
+            this.host += ":"+window.location.port
+        }
+    }
+
+    allServices(){
+		console.log("debut play")
+        let method : string = '/allServices/'
+        let urlws : string= this.host + method;
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+        console.log("Acces à l'adresse : "+urlws);
+        return this.http.get(urlws,{headers:headers})
+            .map(res => res.json())
+            .map((res: any) => {
+                return res;
+            })
+    
+	}
+
+}
