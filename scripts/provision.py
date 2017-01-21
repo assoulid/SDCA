b2c8abaaa735a37b9ad72a9c890b9827fcd9c2e1
OthmaneN <nahyl.othmane@gmail.com>
2017-01-21 01:51:08 +0100

    Added support for galera nodes provisionning

--- scripts/provision.py
+++ scripts/provision.py
@@ -14,8 +14,7 @@ W_INSTANCES_COUNT = 1
 P_INSTANCES_COUNT = 1
 S_INSTANCES_COUNT = 1
 I_INSTANCES_COUNT = 1
-S_MYSQL_INSTANCES_COUNT = 1
-MYSQL_INSTANCES_COUNT = 1
+GALERA_INSTANCES_COUNT = 2
 APPLIWEB_INSTANCES_COUNT = 1
 
 subprocess.call(
@@ -25,8 +24,7 @@ subprocess.call(
     "--parameter p_instances_count={} "
     "--parameter s_instances_count={} "
     "--parameter i_instances_count={} "
-    "--parameter s_mysql_instances_count={} "
-    "--parameter mysql_instances_count={} "
+    "--parameter galera_instances_count={} "
     "--parameter consul_server_instances_count={} "
     "--parameter appliWeb_instances_count={}"
         .format(
@@ -36,8 +34,7 @@ subprocess.call(
         P_INSTANCES_COUNT,
         S_INSTANCES_COUNT,
         I_INSTANCES_COUNT,
-        S_MYSQL_INSTANCES_COUNT,
-        MYSQL_INSTANCES_COUNT,
+        GALERA_INSTANCES_COUNT,
         CONSUL_INSTANCES_COUNT,
         APPLIWEB_INSTANCES_COUNT),
     shell=True)
@@ -62,22 +59,25 @@ while stack_status["stack_status"] != "CREATE_COMPLETE":
 
 # Create and associate a floating ip address to appliWeb instances
 
-def create_floating_ip():
-    floating_ip_state = json.loads(
-        subprocess.check_output("openstack floating ip create external-network -f json", shell=True).decode())
-    return floating_ip_state
-
-
-for app_instance_index in range(0, APPLIWEB_INSTANCES_COUNT):
-    current_floating_ip = create_floating_ip()
-    subprocess.check_output(
-        "openstack server add floating ip appliWeb_{} {}".format(app_instance_index,
-                                                                 current_floating_ip["floating_ip_address"]),
-        shell=True)
+# def create_floating_ip():
+#     floating_ip_state = json.loads(
+#         subprocess.check_output("openstack floating ip create external-network -f json", shell=True).decode())
+#     return floating_ip_state
+#
+#
+# for app_instance_index in range(0, APPLIWEB_INSTANCES_COUNT):
+#     current_floating_ip = create_floating_ip()
+#     subprocess.check_output(
+#         "openstack server add floating ip appliWeb_{} {}".format(app_instance_index,
+#                                                                  current_floating_ip["floating_ip_address"]),
+#         shell=True)
 
 print("Stack creation successful.")
 
 # Extract instances ips
+galera_first_node_ip = list(
+    map(lambda x: x["output_value"],
+        filter(lambda x: x["output_key"] == "galera_first_node", stack_status["outputs"])))[0]
 b_instances_ips = list(
     map(lambda x: x["output_value"], filter(lambda x: x["output_key"] == "b", stack_status["outputs"])))[0]
 w_instances_ips = list(
@@ -88,12 +88,11 @@ s_instances_ips = list(
     map(lambda x: x["output_value"], filter(lambda x: x["output_key"] == "s", stack_status["outputs"])))[0]
 i_instances_ips = list(
     map(lambda x: x["output_value"], filter(lambda x: x["output_key"] == "i", stack_status["outputs"])))[0]
-s_mysql_instances_ips = list(
-    map(lambda x: x["output_value"], filter(lambda x: x["output_key"] == "s_mysql", stack_status["outputs"])))[0]
+galera_instances_ips = list(
+    map(lambda x: x["output_value"],
+        filter(lambda x: x["output_key"] == "galera_other_node", stack_status["outputs"])))[0]
 consul_server_instances_ips = list(
     map(lambda x: x["output_value"], filter(lambda x: x["output_key"] == "consul_server", stack_status["outputs"])))[0]
-mysql_instances_ips = list(
-    map(lambda x: x["output_value"], filter(lambda x: x["output_key"] == "mysql", stack_status["outputs"])))[0]
 appliWeb_instances_ips = list(
     map(lambda x: x["output_value"], filter(lambda x: x["output_key"] == "appliWeb", stack_status["outputs"])))[0]
 
@@ -122,13 +121,17 @@ with open("/etc/ansible/hosts", "w") as hosts:
     for cs in consul_server_instances_ips:
         hosts.write(cs + "\n")
 
-    hosts.write("[s_mysql]\n")
-    for sm in s_mysql_instances_ips:
-        hosts.write(sm + "\n")
+    hosts.write("[galera_all_nodes]\n")
+    for gi in galera_instances_ips:
+        hosts.write(gi + "\n")
+    hosts.write(galera_first_node_ip + "\n")
 
-    hosts.write("[mysql]\n")
-    for ms in mysql_instances_ips:
-        hosts.write(ms + "\n")
+    hosts.write("[galera_other_node]\n")
+    for gi in galera_instances_ips:
+        hosts.write(gi + "\n")
+
+    hosts.write("[galera_first_node]\n")
+    hosts.write(galera_first_node_ip + "\n")
 
     hosts.write("[appliWeb]\n")
     for aw in appliWeb_instances_ips:
@@ -136,12 +139,13 @@ with open("/etc/ansible/hosts", "w") as hosts:
 
     hosts.write("[all_nodes]\n")
     for instance in b_instances_ips + w_instances_ips + consul_server_instances_ips \
-            + mysql_instances_ips + appliWeb_instances_ips + p_instances_ips + s_instances_ips + i_instances_ips + s_mysql_instances_ips:
+            + appliWeb_instances_ips + p_instances_ips + s_instances_ips + i_instances_ips:
         hosts.write(instance + "\n")
 
 with open(os.path.expanduser("~/scan_hosts"), "w") as sh:
     for instance in b_instances_ips + w_instances_ips + consul_server_instances_ips \
-            + mysql_instances_ips + appliWeb_instances_ips + p_instances_ips + s_instances_ips + i_instances_ips + s_mysql_instances_ips:
+            + appliWeb_instances_ips + p_instances_ips + s_instances_ips + i_instances_ips + galera_instances_ips:
         sh.write("ssh-keyscan -t rsa {} >> ~/.ssh/known_hosts\n".format(instance))
+    sh.write("ssh-keyscan -t rsa {} >> ~/.ssh/known_hosts\n".format(galera_first_node_ip))
 
 os.chmod(os.path.expanduser("~/scan_hosts"), 0o777)
